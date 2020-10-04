542ebeacf6180a0f06b8bbebade424dee0bd8f55
TOM_RUS <tomrus88@gmail.com>
2020-10-04 11:54:21 +0300

    Upgrade to .NET 5.0

--- WowUp.WPF/Utilities/AppUtilities.cs
+++ WowUp.WPF/Utilities/AppUtilities.cs
@@ -7,13 +7,13 @@ namespace WowUp.WPF.Utilities
 {
     public static class AppUtilities
     {
-        public static string LongVersionName => FileVersionInfo.GetVersionInfo(typeof(App).Assembly.Location).ProductVersion;
+        public static string LongVersionName => FileVersionInfo.GetVersionInfo(ApplicationFilePath).ProductVersion;
         public static bool IsBetaBuild => LongVersionName.Contains("beta", StringComparison.OrdinalIgnoreCase);
         public static Version CurrentVersion => typeof(App).Assembly.GetName().Version;
-        public static string CurrentVersionString => typeof(App).Assembly.GetName().Version.ToString();
+        public static string CurrentVersionString => CurrentVersion.ToString();
         public static string ApplicationFilePath => Process.GetCurrentProcess().MainModule.FileName;
         public static string ApplicationFileName => Path.GetFileName(ApplicationFilePath);
-        public static int ApplicationProcessId => Process.GetCurrentProcess().Id;
+        public static int ApplicationProcessId => Environment.ProcessId;
         public static string ApplicationProcessName => Process.GetCurrentProcess().ProcessName;
 
         public static void ShutdownApplication()
